d859e765bed1fb57c7eb33d0c367eb64b129b8dd
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2020-11-03 22:18:00 +0200

    Wrapped in async functions

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const core = require('@actions/core');
 const io = require('@actions/io');
 
 
-const folderToCache = core.getInput('folder-to-cache', { required: true });
-const cacheTargetFolder = core.getInput('storage-folder', { required: true })
+async function action() {
+    const folderToCache = core.getInput('folder-to-cache', { required: true });
+    const cacheTargetFolder = core.getInput('storage-folder', { required: true })
 
-core.info(`Attempting to restore cache from ${cacheTargetFolder} to ${folderToCache}`)
+    core.info(`Attempting to restore cache from ${cacheTargetFolder} to ${folderToCache}`)
 
-await io.cp(cacheTargetFolder, folderToCache, { recursive: true, force: false });
+    await io.cp(cacheTargetFolder, folderToCache, { recursive: true, force: false });
 
-core.info(`Restored cache from ${cacheTargetFolder} to ${folderToCache}`)
+    core.info(`Restored cache from ${cacheTargetFolder} to ${folderToCache}`)
+}
+
+return action.then(() => {
+    console.log('finished with local-cache-action')
+})
 
 
